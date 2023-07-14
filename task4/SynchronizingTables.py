ff6b05fd02ca9544605a1147908fae3d074e956b
Tiana Skorokhodova <48140927+TianaSkorokhodova@users.noreply.github.com>
2023-07-14 22:52:53 +0300

    Update SynchronizingTables.py

--- task4/SynchronizingTables.py
+++ task4/SynchronizingTables.py
@@ -1,1 +1,12 @@
-
+def SynchronizingTables(N, ids, salary):
+    
+    ids_sort = sorted(ids)
+    salary_sort = sorted(salary)
+    
+    salary_sort_new = []
+    
+    for i in range (N):
+    
+        salary_sort_new.append(salary_sort[ids_sort.index(ids[i])])
+    
+    return salary_sort_new
