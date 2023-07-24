de6e117e46f04c83b604cd96d4be83b9c8b6b8c5
Tiana Skorokhodova <48140927+TianaSkorokhodova@users.noreply.github.com>
2023-07-24 23:11:48 +0300

    Update UFO.py

--- task11/UFO.py
+++ task11/UFO.py
@@ -1,1 +1,17 @@
+def UFO (N, data, octal) :
+    
+    data10 = []
+    if octal :
+        base = 8
+    else :     
+        base = 16
 
+    for i in range (len(data)) :
+        dataStr = str (data[i])
+        number = 0
+    
+        for j in range (len(dataStr)) :    
+            number += int(dataStr[j]) * (base ** (len(dataStr) - 1 - j))
+        data10.append(number)
+        
+    return data10  
